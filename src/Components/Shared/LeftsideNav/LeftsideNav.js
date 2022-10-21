5554dd58e6673446f969b4b4ed946cfaef5cdcb3
Arif351 <ariful8836@gmail.com>
2022-10-21 20:38:12 +0600

    adfa

--- src/Components/Shared/LeftsideNav/LeftsideNav.js
+++ src/Components/Shared/LeftsideNav/LeftsideNav.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Category from '../../Pages/Category/Category';
 
 const LeftsideNav = () => {
     const [Category, setCategory] = useState([]);
 
-
+    useEffect(() => {
+        fetch('http://localhost:5000/newsCategories')
+            .then(res => res.json())
+            .then(data => setCategory(data))
+    }, [])
 
     return (
         <div>
-            <h2>News Category</h2>
-        </div>
+            <h2>News Category {Category.length}</h2>
+            {
+                Category.map(cat => <p key={cat.id}>
+                    <Link to={`/category/${cat.id}`}> {cat.name} </Link>
+                </p>)
+            }
+        </div >
     );
 };
 
--- src/Components/Shared/RightSideNav/RightSideNav.js
+++ src/Components/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaWhatsapp, FaTwitter, FaYoutube } from "react-icons/fa";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Carusel from '../Carusel/Carusel';
+
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side Nav</h2>
+            <ButtonGroup vertical>
+                <Button variant="outline-primary"> <FaGoogle></FaGoogle> Log in with Google</Button>{' '}
+                <Button variant="outline-secondary"><FaGithub></FaGithub> Log in with GitHub</Button>{' '}
+            </ButtonGroup>
+
+            <div className='mt-4'>
+                <h5>Find us on</h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'> <FaFacebook></FaFacebook> FaceBook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'> <FaWhatsapp></FaWhatsapp> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter></FaTwitter> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaYoutube></FaYoutube> YouTube</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <Carusel></Carusel>
+            </div>
         </div>
     );
 };
